f9ee3bbb2446ebfbbbe4e509c56f0ecace5e2d42
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-10 13:57:10 +0200

    header done, main done, responsive done, wait for footer

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+
+    <div class="container-fluid">
+      <div class="row w-100">
+
+        <div class="enterprise col-md-6 col-xs-6">
+          <a href="index.php">iShoesYou</a>
+        </div>
+
+        <div class="main col-md-6 col-xs-6">
+          <a href="index.php">Accueil</a>
+        </div>
+    </div>
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>iShoesYou</title>
+  <meta name="description" content="Basket e-commerce, discover our baskets">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include("header.php"); ?>
+
+<div class="container-fluid my-5">
+  <div class="row">
+
+      <div class="product col-md-3 col-sm-6">
+        <a href="product-dunk.php">
+          <img class="img-fluid" width="250" src="img/basketdunk.jpg">
+        </a>
+          <div class="spec">
+            <p>Basket-Dunk</p>
+            <p class="price">80 €</p>
+        </div>
+      </div>
+
+      <div class="product col-md-3 col-sm-6">
+        <a href="product-ceyue.php">
+          <img class="img-fluid" width="250" src="img/ceyue.jpg">
+        </a>
+          <div class="spec">
+            <p>Basket-Ceyue</p>
+            <p class="price"> 120 €</p>
+          </div>
+      </div>
+
+      <div class="product col-md-3 col-sm-6">
+        <a href="product-spider.php">
+          <img class="img-fluid" width="250" src="img/spider.jpg">
+        </a>
+        <div class="spec">
+          <p>Basket-Spider</p>
+          <p class="price">90 €</p>
+        </div>
+      </div>
+
+      <div class="product col-md-3 col-sm-6">
+        <a href ="product-reb.php">
+          <img class="img-fluid" width="250" src="img/reb.jpg">
+        </a>
+        <div class="spec">
+          <p>Basket-Reb</p>
+          <p class="price">150 €</p>
+        </div>
+      </div>
+  </div>
+</div>
+
+
+<!-- <?php include("footer.php"); ?> -->
+
+
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
