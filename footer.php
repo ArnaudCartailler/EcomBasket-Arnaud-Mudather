44acb027e17780e7daafea0211df935fa84bbd71
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-10 15:50:41 +0200

    editing header and footer colors

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
         <div class="social">
 
           <div class="twitter">
-            <a href=""><i class="fab fa-twitter fa-3x"></i></a>
+            <a href=""><i class="fab fa-twitter fa-2x"></i></a>
           </div>
 
           <div class="facebook">
-            <a href=""><i class="fab fa-facebook fa-3x"></i></a>
+            <a href=""><i class="fab fa-facebook fa-2x"></i></a>
           </div>
 
       </div>
