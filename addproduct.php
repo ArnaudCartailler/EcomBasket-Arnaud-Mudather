d78c5a67ed8603c7fc4d7d58147e8d599ce83869
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-17 15:45:36 +0200

    working on adding in DB

---
+++ addproduct.php
@@ -0,0 +1,68 @@
+<?php include("header.php"); ?>
+
+
+        <form action="addproduct.php" method="post" enctype="multipart/form-data">
+
+                          <p>Veuillez taper le nom du produit:</p>
+                            <input type="text" name="name" />
+
+                          <p>Veuillez taper le prix :</p>
+                            <input type="text" name="price" />
+
+                            <p>Veuillez taper la taille:</p>
+                              <input type="text" name="size" />
+
+                            <p>Veuillez rentrer la couleur:</p>
+                              <input type="text" name="color" />
+
+                            <p>Veuillez décrire le produit :</p>
+                              <input type="text" name="sum" />
+
+                            <p>Valider :</p>
+                              <input type="submit" name="valid" value="Envoyer le formulaire" />
+             </form>
+
+             <?php
+
+           session_start();
+
+                 try{
+
+                 $bdd = new PDO('mysql:host=localhost;dbname=ecom;charset=utf8', 'root', 'soleil1993');
+
+                 }
+
+                 catch(Exception $e){
+
+                       die('Erreur : '.$e->getMessage());
+                 }
+
+           // Attempt insert query execution
+           try{
+
+               // Create prepared statement
+               $sql = "INSERT INTO goods (name, price, size, color, sum) VALUES (:name, :price, :size, :color, :sum)";
+               $stmt = $bdd->prepare($sql);
+
+               // Bind parameters to statement
+               $stmt->bindParam(':name', $_POST['name']);
+               $stmt->bindParam(':price', $_POST['price']);
+               $stmt->bindParam(':size', $_POST['size']);
+               $stmt->bindParam(':color', $_POST['color']);
+               $stmt->bindParam(':sum', $_POST['sum']);
+
+               // Execute the prepared statement
+               $stmt->execute();
+
+               echo "Records inserted successfully.";
+
+           } catch(PDOException $e){
+
+               die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+           }
+
+           // Close connection
+           unset($bdd);
+
+           ?>
+             <?php include("footer.php"); ?>
--- index.php
+++ index.php
@@ -3,22 +3,19 @@
 
 <?php include("data.php"); ?>
 
-
 <div class="container-fluid my-5">
   <div class="row">
 
     <?php foreach($products as $key => $value){ ?>
-
       <div class="product col-md-3 col-sm-6">
-        <a href="product.php?index=<?php echo $key; ?>">
+        <a class="link_product" href="product.php?index=<?php echo $key; ?>">
           <img class="img-fluid" width="250" src="<?php echo $value['src'] ?>" alt="basketdunk">
-        </a>
           <div class="spec">
             <p><?php echo $value['name'] ?></p>
             <p class="price"><?php echo $value['price'] ?></p>
         </div>
       </div>
-
+    </a>
       <?php
 
       }
