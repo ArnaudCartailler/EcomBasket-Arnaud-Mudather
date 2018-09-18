12191e1b76961d88c928d0afb01e8d2dc40596ab
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-09-18 11:40:43 +0200

    add sql ecom

--- ecom.sql
+++ ecom.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 18 Septembre 2018 à 09:25
+-- Généré le :  Mar 18 Septembre 2018 à 11:40
 -- Version du serveur :  5.7.23-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.30-0ubuntu0.16.04.1
 
@@ -19,6 +19,25 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `ecom`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `pseudo`, `pass`) VALUES
+(1, 'Petitpain', 'coco59');
 
 -- --------------------------------------------------------
 
@@ -46,9 +65,34 @@ INSERT INTO `goods` (`id`, `name`, `price`, `size`, `color`, `stock`, `summary`)
 (5, 'Basket-Spider', 90, 43, 'White', 0, 'Produit fait pour la sortie de spider-man'),
 (6, 'Basket-Reb', 150, 43, 'White', 1, 'Produit fait pour craner');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `information`
+--
+
+CREATE TABLE `information` (
+  `id` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `information`
+--
+
+INSERT INTO `information` (`id`, `Title`, `author`) VALUES
+(1, 'iShoesYou', 'Arnaud Cartailler');
+
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `goods`
@@ -57,14 +101,30 @@ ALTER TABLE `goods`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `information`
+--
+ALTER TABLE `information`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT pour la table `goods`
 --
 ALTER TABLE `goods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT pour la table `information`
+--
+ALTER TABLE `information`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
